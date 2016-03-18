e63d049f879ec94d0979e00a5229903ff27d61f5
NorbiPeti <szatmari.norbert.peter@gmail.com>
2016-03-18 22:41:11 +0100

    Added files

---
+++ PingerBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using DiscordSharp;
+using DiscordSharp.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PingerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscordClient client = new DiscordClient();
+            Console.Write("Username: ");
+            client.ClientPrivateInformation.email = Console.ReadLine().Trim();
+            Console.Write("Password: ");
+            client.ClientPrivateInformation.password = Console.ReadLine().Trim();
+
+            client.Connected += (sender, e) =>
+            {
+                Console.WriteLine($"Connected! User: {e.user.Email}");
+            };
+            try
+            {
+                client.SendLoginRequest();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            client.GetTextClientLogger.LogMessageReceived += delegate (object sender, LoggerMessageReceivedArgs e)
+              {
+                  Console.WriteLine(e.message.Message);
+              };
+            /*Thread t = new Thread(client.Connect);
+            t.Start();*/
+            client.Connect();
+            client.MessageReceived += delegate (object sender, DiscordMessageEventArgs e)
+            {
+                Console.WriteLine(e.message_text);
+                if (e.Channel.Name != "general" && e.Channel.Name != "music-shitpost")
+                    return;
+                if (e.message_text.Contains("<@126012419792306177>") && e.author.ID != "126012419792306177")
+                {
+                    client.SendMessageToChannel("Stop pinging NorbiPeti, <@" + e.author.ID + ">", e.Channel);
+                }
+            };
+            Console.ReadLine();
+        }
+    }
+}
